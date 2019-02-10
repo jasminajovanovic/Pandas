6ee347e1429ad526285ea0d281f126c9cab28a4b
Jasmina Jovanovic <jasminajancic@gmail.com>
2019-02-09 19:39:45 -0600

    changed variable names to clean code format

--- PyCitySchools/main.py
+++ PyCitySchools/main.py
@@ -8,76 +8,76 @@ import numpy as np
 from IPython.display import display
 
 # File to Load (Remember to Change These)
-school_data_to_load = "Resources/schools_complete.csv"
-student_data_to_load = "Resources/students_complete.csv"
+schoolDataToLoad = "Resources/schools_complete.csv"
+studentDataToLoad = "Resources/students_complete.csv"
 
 # Read School and Student Data File and store into Pandas Data Frames
-school_data = pd.read_csv(school_data_to_load)
-student_data = pd.read_csv(student_data_to_load)
+schoolData = pd.read_csv(schoolDataToLoad)
+studentData = pd.read_csv(studentDataToLoad)
 # Combine the data into a single dataset
-school_data_complete = pd.merge(student_data, school_data, how="left", on=["school_name", "school_name"])
+schoolDataComplete = pd.merge(studentData, schoolData, how="left", on=["school_name", "school_name"])
 
-def summarize_district(school_data_complete, school_data, student_data):
+def summarize_district(schoolDataComplete, schoolData, studentData):
 # TODO: improve this code like with schools summary
-    total_schools = school_data.shape[0]
-    total_students = student_data.shape[0]
-    total_budget = school_data['budget'].sum()
-    total_math_pass = dict((student_data["math_score"]>70).value_counts())[True]
-    total_reading_pass = dict((student_data["reading_score"]>70).value_counts())[True]
-    average_math_score = student_data['math_score'].mean()
-    average_reading_score = student_data['reading_score'].mean()
-    percent_math_pass = round(total_math_pass/total_students*100, 2)
-    percent_reading_pass = round(total_reading_pass/total_students*100, 2)
+    totalSchools = schoolData.shape[0]
+    totalStudents = studentData.shape[0]
+    totalBudget = schoolData['budget'].sum()
+    totalMathPass = dict((studentData["math_score"]>70).value_counts())[True]
+    totalReadingPass = dict((studentData["reading_score"]>70).value_counts())[True]
+    averageMathScore = studentData['math_score'].mean()
+    averageReadingScore = studentData['reading_score'].mean()
+    percentMathPass = round(totalMathPass/totalStudents*100, 2)
+    percentReadingPass = round(totalReadingPass/totalStudents*100, 2)
 
-    district_summary = pd.DataFrame(
-        {'Total Schools' : [total_schools],
-        'Total Students' : [total_students],
-        'Total Budget' : [total_budget],
-        'Per Student Budget' : [round(total_budget/total_students, 2)],
-        'Average Math Score' : [round(average_math_score, 2)],
-        'Average Reading Score': [round(average_reading_score,2)],
-        '% Students Pass Math' : [percent_math_pass],
-        '% Students Pass Reading' : [percent_reading_pass],
-        'Average Pass Rate' : [(percent_math_pass+percent_reading_pass)/2]})
+    districtSummary = pd.DataFrame(
+        {'Total Schools' : [totalSchools],
+        'Total Students' : [totalStudents],
+        'Total Budget' : [totalBudget],
+        'Per Student Budget' : [round(totalBudget/totalStudents, 2)],
+        'Average Math Score' : [round(averageMathScore, 2)],
+        'Average Reading Score': [round(averageReadingScore,2)],
+        '% Students Pass Math' : [percentMathPass],
+        '% Students Pass Reading' : [percentReadingPass],
+        'Average Pass Rate' : [(percentMathPass+percentReadingPass)/2]})
 
-    return district_summary
+    return districtSummary
 
-def summarize_schools(school_data_complete, school_data, student_data):
+def summarize_schools(schoolDataComplete, schoolData, studentData):
     schools = []
-    for high_school in school_data['school_name']:
-        this_school_table = school_data_complete[school_data_complete['school_name'] == high_school]
-        _, _, _, _, school_name, _, _, id, type, students, budget = this_school_table.iloc[0]
-        percent_math_pass = round(this_school_table[this_school_table['math_score']>70].shape[0]/students*100, 2)
-        percent_reading_pass = round(this_school_table[this_school_table['math_score']>70].shape[0]/students*100, 2)
+    for highSchool in schoolData['school_name']:
+        thisSchoolTable = schoolDataComplete[schoolDataComplete['school_name'] == highSchool]
+        _, _, _, _, schoolName, _, _, id, type, students, budget = thisSchoolTable.iloc[0]
+        percentMathPass = round(thisSchoolTable[thisSchoolTable['math_score']>70].shape[0]/students*100, 2)
+        percentReadingPass = round(thisSchoolTable[thisSchoolTable['math_score']>70].shape[0]/students*100, 2)
 
         schools.append (
-            {'School Name' : school_name,
+            {'School Name' : schoolName,
             'School Type' : type,
             'Total Students' : students,
             'Total School Budget' : budget,
             'Per Student Budget' : budget/students,
-            'Average Math Score' : round(this_school_table['math_score'].mean(), 2),
-            'Average Reading Score' : round(this_school_table['reading_score'].mean(), 2),
-            '% Passing Math' : percent_math_pass,
-            '% Passing Reading' : percent_reading_pass,
-            'Overall Passing Rate': (percent_math_pass + percent_reading_pass)/2
+            'Average Math Score' : round(thisSchoolTable['math_score'].mean(), 2),
+            'Average Reading Score' : round(thisSchoolTable['reading_score'].mean(), 2),
+            '% Passing Math' : percentMathPass,
+            '% Passing Reading' : percentReadingPass,
+            'Overall Passing Rate': (percentMathPass + percentReadingPass)/2
             })
-    schools_summary = pd.DataFrame (schools)
-    schools_summary = schools_summary[['School Name', 'School Type', "Total Students", 'Total School Budget', 'Per Student Budget', 'Average Math Score', 'Average Reading Score', '% Passing Math', '% Passing Reading', 'Overall Passing Rate']]
-    return schools_summary
+    schoolsSummary = pd.DataFrame (schools)
+    schoolsSummary = schoolsSummary[['School Name', 'School Type', "Total Students", 'Total School Budget', 'Per Student Budget', 'Average Math Score', 'Average Reading Score', '% Passing Math', '% Passing Reading', 'Overall Passing Rate']]
+    return schoolsSummary
 
 
-district_summary = summarize_district(school_data_complete, school_data, student_data)
-schools_summary = summarize_schools(school_data_complete, school_data, student_data)
+districtSummary = summarize_district(schoolDataComplete, schoolData, studentData)
+schoolsSummary = summarize_schools(schoolDataComplete, schoolData, studentData)
 
 # Display only relevant columns
-display(schools_summary [['Per Student Budget', 'Total Students', 'Overall Passing Rate']])
-display(district_summary [['Total Schools', 'Total Students', 'Per Student Budget', '% Students Pass Math', '% Students Pass Reading', 'Average Pass Rate']])
+display(schoolsSummary [['Per Student Budget', 'Total Students', 'Overall Passing Rate']])
+display(districtSummary [['Total Schools', 'Total Students', 'Per Student Budget', '% Students Pass Math', '% Students Pass Reading', 'Average Pass Rate']])
 
 print ("--------------- Top 5 best performing schools -----------------------------------")
-display(schools_summary.nlargest(5, 'Overall Passing Rate'))
+display(schoolsSummary.nlargest(5, 'Overall Passing Rate'))
 print ("--------------- Bottom 5 worst performing schools -----------------------------------")
-display(schools_summary.nsmallest(5, 'Overall Passing Rate'))
+display(schoolsSummary.nsmallest(5, 'Overall Passing Rate'))
 
 # spending_bins = [0, 585, 615, 645, 675]
 # group_names = ["<$585", "$585-615", "$615-645", "$645-675"]
